b2a59bcba756977e5c33700f5f7be4dd5bc13c70
Rafael César(RaYaS) <rafael.cesar.souza@hotmail.com>
2025-05-04 17:46:07 -0300

    validação de placa Modelo antigo AAA-1111

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -11,21 +11,57 @@ namespace DesafioFundamentos.Models
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
         }
+        // Listas para armazenar letras e números para a validação da placa modelo antigo
+        List<string> letters = [ "A", "B", "C", "D", "E", "F", "G",
+                                "H", "I", "J", "K", "L", "M", "N",
+                                "O", "P", "Q", "R", "S", "T", "U",
+                                "V", "W", "X", "Y", "Z"];
 
+        List<string> numbers = ["1","2","3","4","5","6","7","8","9","0"];
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.WriteLine("Digite a placa do veículo para estacionar(Ex: AAA-1111):");
             string carro = Console.ReadLine();
-            string pattern = @"^\d{3}-\d{4}";
+            string pattern = @"^[A-Za-z]{3}-\d{4}";
             
             if(System.Text.RegularExpressions.Regex.IsMatch(carro,pattern))
             {
-                 veiculos.Add(carro);
+                //variaveis para gerar subStrings, pegando os valores das letras e numeros 
+                string lettersCar = carro[..3];
+                string numbersCar = carro[4..];
+
+                // contadores para validar a placa
+                int countLetters = 0;
+                int countNumbers = 0;
+                for(int i = 0; i < lettersCar.Length; i++)
+                {
+                    if(letters.Contains(lettersCar[i].ToString().ToUpper()))
+                    {
+                        countLetters++;
+                    }
+                }
+
+                for(int i =0; i < numbersCar.Length; i++)
+                {
+                    if(numbers.Contains(numbersCar[i].ToString()))
+                    {
+                        countNumbers++;
+                    }
+                }
+                    if(countLetters==3 && countNumbers==4)
+                    {
+                        veiculos.Add(carro);
+                        Console.WriteLine("Carro cadastrado com sucesso!");
+                    }else
+                    {
+                        Console.WriteLine("Placa invalida, use o padrão AAA-1111");
+                    }                
+
             }else
             {
-                Console.WriteLine("Placa invalida");
+                Console.WriteLine("Placa invalida, use o padrão AAA-1111");
             }
         }
 
@@ -76,7 +112,7 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
-                int count = 0 ;
+                int count = 1 ;
                 foreach(string car in veiculos)
                 {
                     Console.WriteLine($"{count} - {car.ToUpper()}");
